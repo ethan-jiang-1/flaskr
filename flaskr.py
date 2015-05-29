ada513f079433ba3e1e63d03588aa7683e23a378
ethan <ethan1.jiang@gmail.com>
2015-05-29 18:33:35 +0800

    fix few issues

--- flaskr.py
+++ flaskr.py
@@ -39,7 +39,35 @@ def show_entires():
     rdt = render_template('show_entries.html', entires=entries)
     return rdt 
 
+@app.route('/add',methods=['POST'])
+def add_entry():
+	if not session.get('logged_in'):
+		abort(401)
+	g.db.execute('insert into entries (title,text) values (?,?)', [request.form['title'],request.form['text']])
+	g.db.commit()
+	flash('New entry was successfuly posted')
+	redirect(url_for('show_entries'))
 
+@app.route('/login',methods=['GET','POST'])
+def login():
+	error = None
+	if request.method == 'POST':
+		if request.form['username'] != app.config['USERNAME']:
+			error = 'Invalid username'
+		elif request.form['password'] != app.config['PASSWORD']:
+			error = 'Invalid password'
+		else:
+			session['logged_in'] = True
+			flash('You were logged in')
+			return redirect(url_for('show_entries'))
+	return render_template('login.html',error=error)
+
+@app.route('/logout')
+def logout():
+	session.pop('logged_in',None)
+	flash('You were logged out')
+	return redirect(url_for('show_entries'))
+	
 
 if __name__ == '__main__':
 	app.run() 
