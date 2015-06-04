afd419619199732ad6ac7cf2add77c95f9d9b448
ethan <ethan1.jiang@gmail.com>
2015-06-04 18:44:08 +0800

    test plus some fixes

--- flaskr.py
+++ flaskr.py
@@ -4,6 +4,7 @@
 import pdb
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+from contextlib import closing
 
 #configuration
 DATABASE = '/tmp/flaskr.db'
@@ -17,13 +18,19 @@ PASSWORD = 'default'
 app = Flask(__name__)
 app.config.from_object(__name__)
 
-def connnect_db():
+def connect_db():
 	return sqlite3.connect(app.config['DATABASE'])
+
+def init_db():
+	with closing(connect_db()) as db:
+		with app.open_resource('schema.sql',mode='r') as f:
+			db.cursor().executescript(f.read())
+		db.commit()
 
 
 @app.before_request
 def before_request():
-	g.db = connnect_db() 
+	g.db = connect_db() 
 
 @app.teardown_request
 def teardown_request(exception):
@@ -36,17 +43,18 @@ def show_entries():
     cur = g.db.execute('select title, text from entries order by id desc')
     entries = [dict(title=row[0],text=row[1]) for row in cur.fetchall()]
     #pdb.set_trace()
-    rdt = render_template('show_entries.html', entires=entries)
+    rdt = render_template('show_entries.html', entries=entries)
     return rdt 
 
 @app.route('/add',methods=['POST'])
 def add_entry():
+	#pdb.set_trace()
 	if not session.get('logged_in'):
 		abort(401)
 	g.db.execute('insert into entries (title,text) values (?,?)', [request.form['title'],request.form['text']])
 	g.db.commit()
 	flash('New entry was successfuly posted')
-	redirect(url_for('show_entries'))
+	return redirect(url_for('show_entries'))
 
 @app.route('/login',methods=['GET','POST'])
 def login():
